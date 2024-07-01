5fd06b617fd144685b534b4ddff57289deb13fa6
lucas <lucassidpaula@gmail.com>
2024-07-01 00:25:24 -0300

    mais uma

--- exe9.js
+++ exe9.js
@@ -7,12 +7,25 @@ altura = parseInt(altura);
 
 let verifiação = false;
 let sexualidaefemia;
-sexualidade2 = prompt("coloque aqui sua sexualidade");
-sexualidade2 = sexualidade2.toLocaleLowerCase();
+let sexualidademasculina;
 while (!verifiação) {
+  sexualidade2 = prompt("coloque aqui sua sexualidade");
+  sexualidade2 = sexualidade2.toLocaleLowerCase();
   if (sexualidade2 === "f" || sexualidade2 === "m") {
     verifiação = true;
+    if (sexualidade2 === "f") {
+      sexualidaefemia = sexualidade2;
+    } else {
+      sexualidademasculina = sexualidade2;
+    }
   } else {
     verifiação;
   }
+}
+if (sexualidaefemia) {
+  const pesoidealfeminino = 61.1 * altura - 44.7;
+  console.log(pesoidealfeminino);
+} else {
+  //  coloca o resto.
+  console.log("resto");
 }
